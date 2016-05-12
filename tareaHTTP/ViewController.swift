fb37f4a6bff3b90cb51d95649e32bb9e846ae356
Nicolas Roman <nicoroman@gmail.com>
2016-05-12 13:28:00 -0500

    Primer commit

--- tareaHTTP/ViewController.swift
+++ tareaHTTP/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var textoISBN: UITextField!
+    @IBOutlet weak var textoRespuesta: UITextView!
+    var textoDisplay: String?
+    
+    // Funciones
+    
+    func sincrono(isbn:String) {
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+isbn
+        let url = NSURL(string: urls)
+        let datos: NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        textoDisplay = String(texto)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textoRespuesta.text = "Acá se mostrará el texto del ISBN"
     }
-
+    
+    // MARK: Actions
+    @IBAction func buscaISBN(sender: UITextField) {
+        sincrono(textoISBN.text!)
+        textoRespuesta.text = textoDisplay
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
